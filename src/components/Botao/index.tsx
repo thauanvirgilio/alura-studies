c8292843512de31d3827cc132d60de9f56a238c7
thauan <thauan.virgilio@iob.com.br>
2023-10-16 17:28:36 -0300

    criando-projeto

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Formulario from '../Formulario';
+
+class Botao extends React.Component {
+    render() {
+        return (
+            <button>
+                Botão
+            </button>
+        )
+    }
+}
+
+export default Botao;
